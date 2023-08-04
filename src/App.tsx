f56035f7d0deb7d5d9d5f7f42646ec53027cbdf1
CTDKSKM <skmdldy001@gmail.com>
2023-08-04 15:35:29 +0900

    로그인기능 테스트완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import Router from "./shared/Router";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 const App = () => {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    });
+  }, []);
   return <Router />;
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,71 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import styled from "styled-components";
+import { auth } from "../firebase";
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <StyledDiv>TodoList Maker</StyledDiv>;
+  const testSignUp = async () => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        "test@test.com",
+        "111111"
+      );
+      const user = userCredential.user;
+      console.log(user);
+    } catch (error: any) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("errorCode", errorCode);
+      console.log("errorMessage", errorMessage);
+    }
+  };
+  const testLogin = async () => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        "test@test.com",
+        "111111"
+      );
+    } catch (error: any) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    }
+  };
+  const testLogout = async () => {
+    await signOut(auth);
+  };
+  return (
+    <StyledDiv>
+      TodoList Maker
+      <button
+        onClick={() => {
+          testSignUp();
+        }}
+      >
+        회원가입
+      </button>
+      <button
+        onClick={() => {
+          testLogin();
+        }}
+      >
+        로그인
+      </button>
+      <button
+        onClick={() => {
+          testLogout();
+        }}
+      >
+        로그아웃
+      </button>
+    </StyledDiv>
+  );
 };
 
 export default Header;
